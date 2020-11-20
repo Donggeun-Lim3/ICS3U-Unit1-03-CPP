9c275a781a1f62498e010e6856bba68d2d5411c9
Ubuntu <ubuntu@ip-172-31-91-206.ec2.internal>
2020-11-20 19:24:30 +0000

    initial message

---
+++ hello_world.cpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2002 Donggeun Lim All rights reserved
+//
+// Created by Donggeun Lim
+// Created on November 2020
+// This is a "Hello world" program with proper style
+
+#include <iostream>
+
+int main() {
+std::cout << "Hello, World!" << std::endl;
+}
